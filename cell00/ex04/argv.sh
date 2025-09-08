c3143d8772b9123ddc0d89542c0ad5cc0c2db47e
Thitiwut Khrutchuen. <khanun.krootchuen@gmail.com>
2025-09-08 23:21:29 +0700

    update cell 1

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,8 @@
+# input 3 char
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for i in "$@"; do
+        echo "$i"
+    done | head -n 3
+fi
---
+++ cell00/ex05/build.sh
@@ -0,0 +1,12 @@
+# input folder number
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+for arg in "$@"; do
+    dir="ex$arg"
+    if [ ! -d "$dir" ]; then
+        mkdir "$dir"
+    fi
+done
